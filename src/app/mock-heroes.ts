7c679e5042a6bdc03bfdf49696f210882cf15aa7
Arjun Das T P <arjun.das@fantacode.com>
2024-06-11 14:43:06 +0530

    first

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import {Hero} from './hero'
+export const HEROES:Hero[]=[
+    {id:12,name:'Dr santhosh'},
+    {id:14,name:'Bombalo'},
+    {id:10,name:'Dynama'},
+    {id:23,name:'Dr Isac'},
+    {id:123,name:'Asuthosh Mehta'}
+];
